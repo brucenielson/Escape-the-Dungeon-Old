3ac24a2e99974c8fbf4365780791bab3b503768d
brucenielson <brucenielson1@gmail.com>
2017-04-08 13:14:22 -0600

    Finally got skeleton to reel back when his sword is parried.

---
+++ Assets/Scripts/RootMotionScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class RootMotionScript : MonoBehaviour {
+
+	void OnAnimatorMove()
+	{
+		Animator animator = GetComponent<Animator>(); 
+
+		if (animator && animator.GetBool("isWalking"))
+		{
+			this.transform.Translate (0, 0, 0.03f);
+
+			//Vector3 newPosition = transform.position;
+			//newPosition.z += animator.GetFloat("Walkspeed") * Time.deltaTime; 
+			//transform.position = newPosition;
+		}
+	}
+}
+
--- Assets/Scripts/SwordCollision.cs
+++ Assets/Scripts/SwordCollision.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwordCollision : MonoBehaviour {
+	private Animator anim;
 
 	// We just collided with an object
 	private void OnTriggerEnter(Collider collider) {
-		Debug.Log ("Sword Hit something:" + collider.name.ToString());
+		int layer = collider.gameObject.layer;
+		anim = this.GetComponentInParent<Animator> ();
+		//Debug.Log (anim.ToString ());
+		if (layer != LayerMask.NameToLayer ("Floor") && layer != LayerMask.NameToLayer ("Monster")) {
+			Debug.Log ("Sword Hit something:" + collider.name.ToString());
+			Debug.Log (LayerMask.LayerToName(layer));
+
+			anim.SetBool ("isIdle", false);
+			anim.SetBool ("isWalking", false);
+			anim.SetBool ("isAttacking", false);
+			anim.SetBool ("Hit", true);
+		}
 	}
 		
 }
