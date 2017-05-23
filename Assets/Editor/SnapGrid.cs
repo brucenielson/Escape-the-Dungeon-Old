2050c2210a183c8ee1733924beab941bc1a3a3ab
brucenielson <brucenielson1@gmail.com>
2017-05-22 21:11:32 -0600

    Lastest updates. Can't remember what.

---
+++ Assets/Editor/SnapGrid.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+ 
+public class SnapToGrid : ScriptableObject {
+	 
+	[MenuItem ("Window/Snap to Grid %g")]
+	static void MenuSnapToGrid() {
+		foreach (Transform t in Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable)) {
+			float x = Mathf.Round (t.position.x / EditorPrefs.GetFloat ("MoveSnapX")) * EditorPrefs.GetFloat ("MoveSnapX");
+			float y = Mathf.Round (t.position.y / EditorPrefs.GetFloat ("MoveSnapY")) * EditorPrefs.GetFloat ("MoveSnapY");
+			float z = Mathf.Round (t.position.z / EditorPrefs.GetFloat ("MoveSnapZ")) * EditorPrefs.GetFloat ("MoveSnapZ");
+			//Debug.Log ("x: " + x.ToString () + "; y: " + y.ToString () + "; z: " + z.ToString ()); 
+			Vector3 new_pos = new Vector3 (x,y,z);
+			t.position = new_pos;
+		}
+	}
+	 
+}
